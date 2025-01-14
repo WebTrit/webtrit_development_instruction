f6e91447c6c14ee535beca09ce3cc64425f3a246
Roman Yakimenko <r.yakimenko@webtrit.com>
2025-01-14 15:01:38 +0200

    feat: add static page for developer instructions

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const DEFAULT_CORE_HOST = 'core.demo.webtrit.com';
+  const DEFAULT_TENANT_ID = 'default-tenant';
+  const DEFAULT_USERNAME = 'default-username';
+
+  const getQueryParam = (param, defaultValue) => {
+    const params = new URLSearchParams(window.location.search);
+    return params.get(param) || defaultValue;
+  };
+
+  const replacePlaceholder = (placeholder, value) => {
+    const regex = new RegExp(`${placeholder}`, 'g');
+    document.body.innerHTML = document.body.innerHTML.replace(regex, value);
+  }
+
+  replacePlaceholder('{CORE_HOST}', getQueryParam('core', DEFAULT_CORE_HOST));
+  replacePlaceholder('{TENANT_ID}', getQueryParam('tenant_id', DEFAULT_TENANT_ID));
+  replacePlaceholder('{USERNAME}', getQueryParam('username', DEFAULT_USERNAME));
+});
